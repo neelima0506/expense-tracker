3e4b96d6572bcdb195da2d5fce9d42f54da2897b
Neelima Bogadi <150000468+neelima0506@users.noreply.github.com>
2024-12-24 12:44:54 +0530

    Add files via upload

---
+++ create.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect("expenses.db")
+
+
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS expenses
+(id INTEGER PRIMARY KEY,
+Date DATE,
+description text,
+category TEXT,
+price REAL)""")
+
+conn.commit()
+conn.close()
